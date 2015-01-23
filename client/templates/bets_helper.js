c342b69c23f8ea1fad9042da0fb1801040a2d7b2
Kevin Kwon <kvnkwon@gmail.com>
2015-01-23 15:50:25 -0500

    Hide bets that are not selected with the buttons

--- client/templates/bets_helper.js
+++ client/templates/bets_helper.js
@@ -3,3 +3,21 @@ Template.betsList.helpers({
   pending_bets: Bets.find({status: "pending"}),
   completed_bets: Bets.find({status: "completed"})
 });
+
+Template.betsList.events({
+  "click .open-button": function() {
+    $(".open-bets").show();
+    $(".pending-bets").hide();
+    $(".completed-bets").hide();
+  },
+  "click .pending-button": function() {
+    $(".pending-bets").show();
+    $(".open-bets").hide();
+    $(".completed-bets").hide();
+  },
+  "click .completed-button": function() {
+    $(".completed-bets").show();
+    $(".pending-bets").hide();
+    $(".open-bets").hide();
+  }
+})
