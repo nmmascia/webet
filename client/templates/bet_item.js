08ce5418e10ae6d55bf80417c35f180cda31fd31
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-24 09:36:25 -0500

    Template: clicking accept button updates status to pending

--- client/templates/bet_item.js
+++ client/templates/bet_item.js
@@ -1,5 +1,9 @@
 Template.betItem.events({
   'click #remove' : function(){
     Bets.remove(this._id)
+  },
+
+  'click .accept_button' : function() {
+    Bets.update({_id: this._id}, { $set: { status: "pending" }})
   }
 })
