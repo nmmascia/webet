efba5e9da6364c9523c58e8cdc968f52c8fade7e
Nicholas Mascia <nmmascia@gmail.com>
2015-01-24 12:23:26 -0500

    Refactor spacing in template.betsList events for readability.

--- client/templates/bets_helper.js
+++ client/templates/bets_helper.js
@@ -10,16 +10,18 @@ Template.betsList.helpers({
 });
 
 Template.betsList.events({
-  "click .open-button": function() {
-    Session.set("user", Meteor.user().username)
-    Session.set("status", "open")
+  "click .open-button" : function() {
+    Session.set( "user", Meteor.user().username )
+    Session.set( "status", "open" )
   },
-  "click .pending-button": function() {
-    Session.set("user", Meteor.user().username)
-    Session.set("status", "pending")
+
+  "click .pending-button" : function() {
+    Session.set( "user", Meteor.user().username )
+    Session.set( "status", "pending" )
   },
-  "click .completed-button": function() {
-    Session.set("user", Meteor.user().username)
-    Session.set("status", "completed")
+
+  "click .completed-button" : function() {
+    Session.set( "user", Meteor.user().username )
+    Session.set( "status", "completed" )
   }
 })
