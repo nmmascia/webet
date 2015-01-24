e6e0c07b1a00603ce20ec564e3b8e9071e29e118
Kevin <kvnkwon@gmail.com>
2015-01-24 18:13:30 -0500

    fix mc

---
+++ client/templates/bets/singleBet.js
@@ -0,0 +1,31 @@
+Template.singleBet.helpers({
+  openStatus: function() {
+    return ( this.status === "open" ) ? true : false;
+  },
+
+  pendingStatus: function() {
+    return ( this.status === "pending" ) ? true : false;
+  },
+
+  completedStatus: function() {
+    return ( this.status === "completed" ) ? true : false;
+  }
+});
+
+Template.singleBet.events({
+  'click .remove_bet_button' : function(){
+    Bets.remove( this._id );
+  },
+
+  'click .accept_button' : function() {
+    Bets.update({ _id: this._id },
+      { $set: { status: "pending" }
+    });
+  },
+
+  'click .complete_bet_button' : function() {
+    Bets.update({ _id: this._id },
+      { $set: { status: "completed" }
+    });
+  }
+});
