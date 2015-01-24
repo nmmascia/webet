156593550fb26aab46a6fa96c885b4a76af589bb
Mr Fete <d1swaay@gmail.com>
2015-01-24 18:33:53 -0500

    adds tests. Both samples and relative tests for bet creations

---
+++ tests/jasmine/client/integration/CreateBetSpec.js
@@ -0,0 +1,11 @@
+describe("Template.createBetForm.events", function(){
+  it("should create a bet with title", function(){
+    var testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag"})
+    expect(Bets.findOne(testBet).title).toEqual("I bet u fall off that one foot skateboard")
+  });
+
+  it("should create a bet with wager", function(){
+    var testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag"})
+    expect(Bets.findOne(testBet).wager).toEqual("a beach ball in a bag")
+  });
+});
