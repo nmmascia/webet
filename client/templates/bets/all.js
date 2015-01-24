8a0a9cc587878cadb1e1a0a14bdaf77920d1cb69
Nicholas Mascia <nmmascia@gmail.com>
2015-01-24 12:41:27 -0500

    betsList added semicolons to template betsList helper

--- client/templates/bets/all.js
+++ client/templates/bets/all.js
@@ -11,17 +11,17 @@ Template.betsList.helpers({
 
 Template.betsList.events({
   "click .open-button" : function() {
-    Session.set( "user", Meteor.user().username )
-    Session.set( "status", "open" )
+    Session.set( "user", Meteor.user().username );
+    Session.set( "status", "open" );
   },
 
   "click .pending-button" : function() {
-    Session.set( "user", Meteor.user().username )
-    Session.set( "status", "pending" )
+    Session.set( "user", Meteor.user().username );
+    Session.set( "status", "pending" );
   },
 
   "click .completed-button" : function() {
-    Session.set( "user", Meteor.user().username )
-    Session.set( "status", "completed" )
+    Session.set( "user", Meteor.user().username );
+    Session.set( "status", "completed" );
   }
 })
