d09efa148aed1a71b1763b91cf55169b679da341
Nicholas Mascia <nmmascia@gmail.com>
2015-01-24 12:30:24 -0500

    Create Bet: refactor for code clarity and readability.

--- client/templates/create_bet.js
+++ client/templates/create_bet.js
@@ -1,18 +1,17 @@
 Template.createBetForm.events({
   "submit .create-bet": function(event) {
-    var status = "open";
-    var title = event.target.betTitle.value;
-    var wager = event.target.betWager.value;
-    var user = Meteor.user();
-
-    var d_username = event.target.defender.value;
-    var defender = Meteor.users.findOne({ username: d_username })
+    var status = "open",
+        title = event.target.betTitle.value;
+        wager = event.target.betWager.value;
+        user = Meteor.user(),
+        defender_requested = event.target.defender.value;
+        defender = Meteor.users.findOne({ username: defender_requested });
 
     Bets.insert({
-      bettors: [user.username, defender.username],
+      bettors: [ user.username, defender.username ],
       status: status,
       title: title,
       wager: wager
     });
   }
-})
+});
