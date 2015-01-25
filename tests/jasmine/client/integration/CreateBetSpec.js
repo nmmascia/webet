069d603e40201e0c8e5672fe030fd94226ddd25e
Mr Fete <d1swaay@gmail.com>
2015-01-24 19:43:27 -0500

    refactors tests using beforeEach to declare the var

--- tests/jasmine/client/integration/CreateBetSpec.js
+++ tests/jasmine/client/integration/CreateBetSpec.js
@@ -1,16 +1,18 @@
 describe("Template.createBetForm.events", function(){
+  var testBet;
+
+  beforeEach(function(){
+    testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag", status: "open"});
+  });
   it("should create a bet with title", function(){
-    var testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag"})
     expect(Bets.findOne(testBet).title).toEqual("I bet u fall off that one foot skateboard")
   });
 
   it("should create a bet with wager", function(){
-    var testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag"})
     expect(Bets.findOne(testBet).wager).toEqual("a beach ball in a bag")
   });
 
    it("should create a bet with a status", function(){
-    var testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag", status: "open"})
     expect(Bets.findOne(testBet).status).toEqual("open")
   });
 });
