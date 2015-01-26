775313a3ebba806755ff0100b085f5cbb8223b66
fete <d1swaay@gmail.com>
2015-01-25 21:01:40 -0500

    adds beforeEach whichs gives the vars the values

--- tests/jasmine/client/integration/messagesSpec.js
+++ tests/jasmine/client/integration/messagesSpec.js
@@ -2,4 +2,9 @@ describe("Template.messages", function(){
   var testMessage;
   var originalCount;
 
+   beforeEach(function(){
+    originalCount = Messages.find({}).count();
+    testMessage = Messages.insert({message: "Looks like you going to losing this one", sentBy: "nick", bet: "gAeAgX8wbSpiic2mm"});
+   });
+
 });
