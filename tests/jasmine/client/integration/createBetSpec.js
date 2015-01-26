d1c7aed57a1839554527c6fc649b530467b9aeda
fete <d1swaay@gmail.com>
2015-01-25 20:17:59 -0500

    tests created bet has the wager user inputs

--- tests/jasmine/client/integration/createBetSpec.js
+++ tests/jasmine/client/integration/createBetSpec.js
@@ -7,8 +7,12 @@ describe("Template.createBetForm.events", function(){
 	testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag", status: "open"});
   });
 
-  it("should create a bet with title", function(){
+  it("should create a bet with title that user inputs", function(){
     expect(Bets.findOne(testBet).title).toEqual("I bet u fall off that one foot skateboard")
+  });
+
+  it("should create a bet with wager that the user inputs", function(){
+    expect(Bets.findOne(testBet).wager).toEqual("a beach ball in a bag")
   });
 
 });
