55017ff8de652217074e528f4f59479d651e982e
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-25 20:58:51 -0500

    Navigation.js: created js file for navigation

---
+++ client/templates/navigation.js
@@ -0,0 +1,16 @@
+Template.betsList.events({
+  "click .open-button" : function(){
+    Session.set( "user", Meteor.user().username );
+    Session.set( "status", "open" );
+  },
+
+  "click .pending-button" : function(){
+    Session.set( "user", Meteor.user().username );
+    Session.set( "status", "pending" );
+  },
+
+  "click .completed-button" : function(){
+    Session.set( "user", Meteor.user().username );
+    Session.set( "status", "completed" );
+  }
+});
