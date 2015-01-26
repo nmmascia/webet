a0b782ee82d660091479e460246a1126bae518a6
fete <d1swaay@gmail.com>
2015-01-25 20:16:02 -0500

    creates beforeEach method giving values for the vars

--- tests/jasmine/client/integration/createBetSpec.js
+++ tests/jasmine/client/integration/createBetSpec.js
@@ -2,4 +2,9 @@ describe("Template.createBetForm.events", function(){
   var testBet;
   var originalCount;
 
+  beforeEach(function(){
+    originalCount = Bets.find({}).count();
+	testBet = Bets.insert({title: "I bet u fall off that one foot skateboard", wager: "a beach ball in a bag", status: "open"});
+  });
+
 });
