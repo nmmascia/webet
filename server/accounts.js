cf209f3c7a0554a4793e9d04399d8efeb987ac2a
Nicholas Mascia <nmmascia@gmail.com>
2015-01-27 12:07:48 -0500

    Style fix

--- server/accounts.js
+++ server/accounts.js
@@ -1,6 +1,6 @@
 Accounts.onCreateUser(function(options, user) {
   if (options.profile) {
-    options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+    options.profile.picture = 'http://graph.facebook.com/' + user.services.facebook.id + '/picture/?type=large';
     user.profile = options.profile;
   }
   return user;
