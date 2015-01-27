f7d65fa312b6f3ea121bc0f20cc3894c7fe90366
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-27 14:44:37 -0500

    added delete button functionality

---
+++ client/templates/notifications/single.js
@@ -0,0 +1,9 @@
+Template.singleNotification.events({
+  "submit .deleteNotification" : function(event) {
+  event.preventDefault();
+  var username = event.target.toNotify.value)
+
+  Meteor.call("deleteNotification", this._id)
+
+}
+})
