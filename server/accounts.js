a66b635a2341f96242f67b92ae1ef4da3c3d74d8
Nicholas Mascia <nmmascia@gmail.com>
2015-01-27 11:40:58 -0500

    Create onCreate function to grab user's FB profile picture for user dash

---
+++ server/accounts.js
@@ -0,0 +1,7 @@
+Accounts.onCreateUser(function(options, user) {
+  if (options.profile) {
+    options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+    user.profile = options.profile;
+  }
+  return user;
+});
