dae485a5c106b6f6540c465302a6d47e4fe6361c
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-27 17:26:45 -0500

    _stats: created wonBets helper

--- client/templates/user/_stats.js
+++ client/templates/user/_stats.js
@@ -1,5 +1,10 @@
 Template._stats.helpers({
   completedBets: function(){
     return Bets.find({ status: "completed" }).count()
+  },
+
+  wonBets: function(){
+    var user = Session.get("userObject")
+    return Bets.find({ $and: [ { status: "completed" }, { winner: user.username }]}).count();
   }
-})
+});
