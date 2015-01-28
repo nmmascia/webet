c35f5bfd07b72dfa842d4d5017bea13ccfb43b28
Nicholas Mascia <nmmascia@gmail.com>
2015-01-27 20:43:27 -0500

    Quick style fix

--- client/templates/notifications/single.js
+++ client/templates/notifications/single.js
@@ -1,9 +1,8 @@
 Template.singleNotification.events({
   "submit .deleteNotification" : function(event) {
   event.preventDefault();
-  var username = event.target.toNotify.value
+  var username = event.target.toNotify.value;
 
   Meteor.call("deleteNotification", this._id);
-
   }
-})
+});
