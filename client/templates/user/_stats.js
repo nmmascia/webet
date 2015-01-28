640ba656124551a18b3aba663d8a211f5bf80a0a
Nicholas Mascia <nmmascia@gmail.com>
2015-01-27 22:59:41 -0500

    Resolve error in code.

--- client/templates/user/_stats.js
+++ client/templates/user/_stats.js
@@ -3,7 +3,7 @@ Template._stats.helpers({
     var user = Session.get('userObject'),
         completedBets = Bets.find({ status: 'completed' }).count(),
         wins = Bets.find({ winner: user }).count(),
-        var points = Points.findOne({ wonBy: user });
+        points = Points.findOne({ wonBy: user }),
         losses = completedBets - wins;
 
     return [wins, losses]
