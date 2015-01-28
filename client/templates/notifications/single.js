060cc08999bbf90463067dbe2af661b0ecb1707c
Kevin Kwon <kvnkwon@gmail.com>
2015-01-28 17:08:05 -0500

    Notifications delete on click

--- client/templates/notifications/single.js
+++ client/templates/notifications/single.js
@@ -1,8 +1,6 @@
 Template.singleNotification.events({
-  "submit .deleteNotification" : function(event) {
+  "click .deleteNotification" : function(event) {
     event.preventDefault();
-    var username = event.target.toNotify.value;
-
     Meteor.call("deleteNotification", this._id);
   }
 });
