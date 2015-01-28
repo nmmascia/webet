34be2246e63df8680385350d5f92df31f2db8d81
Nicholas Mascia <nmmascia@gmail.com>
2015-01-27 20:41:00 -0500

    Move template helpers to bets.js partial

--- client/templates/user/dashboard.js
+++ client/templates/user/dashboard.js
@@ -1,24 +1,3 @@
-Template._bets.helpers({
-  openBets: function(){
-    return Bets.find({ $and:
-      [ { status: "open" },
-        { bettors: Session.get( "user" ) }
-    ]});
-  },
-  pendingBets: function(){
-    return Bets.find({ $and:
-      [ { status: "pending" },
-        { bettors: Session.get( "user" ) }
-    ]});
-  },
-  completedBets: function(){
-    return Bets.find({ $and:
-      [ { status: "completed" },
-        { bettors: Session.get( "user" ) }
-    ]});
-  }
-});
-
 Template.dashboard.rendered =function() {
     Session.set("userObject", this.data.username)  };
 
